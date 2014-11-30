891dc2ce4ee48370194b0ad2aeb4addc5ab854cd
Niki Hansche <mf-developing@hansche.de>
2014-11-30 14:25:12 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+Shariff-Mediawiki
+=================
+
+A mediawiki-extension for [Shariff](https://github.com/heiseonline/shariff), 
+protecting users privacy.
+
+Setup
+-----
+1. Unpack in the ```extensions```-folder of your mediawiki.
+2. Edit ```Shariff/shariff-backend/shariff.json``` and set the key
+   ```"domain":``` to your domain name.
+3. Add ```require_once("$IP/extensions/Shariff/Shariff.php");``` to
+   ```LocalSettings.php```
+4. Write ```{{#shariffLike:}}``` wherever you want to use social-media-buttons.
+
---
+++ Shariff.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Parser that inserts social media buttons
+ *
+ * For more info see http://mediawiki.org/wiki/Extension:TwitterFBLike
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Niki Hansche
+ * @copyright © 2014 Niki Hansche
+ * @licence The MIT License (MIT)
+ */
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'Shariff', 
+	'author' => 'Niki Hansche', 
+	'url' => 'https://github.com/vonloxley/Shariff-Mediawiki',
+	'description' => 'Insert 2-click "Like" buttons.',
+);
+
+$wgHooks['ParserFirstCallInit'][] = 'shariffLikeParserFunction_Setup';
+$wgHooks['LanguageGetMagic'][]       = 'shariffLikeParserFunction_Magic';
+$wgHooks['BeforePageDisplay'][] = 'shariffLikeParserFeedHead';
+
+function shariffLikeParserFunction_Setup( &$parser ) {
+	# Set a function hook associating the "shariffLike_parser" magic word with our function
+	$parser->setFunctionHook( 'shariffLike', 'shariffLikeParserFunction_Render' );
+	return true;
+}
+ 
+function shariffLikeParserFunction_Magic( &$magicWords, $langCode ) {
+        //Set first parameter to 1 to make it case sensitive
+	$magicWords['shariffLike'] = array( 0, 'ShariffLike' );
+        return true;
+}
+
+function shariffLikeParserFeedHead(&$out, &$sk) {
+	global $wgScriptPath;
+	$out->addHeadItem('shariff.min.css','<link rel="stylesheet" type="text/css" href="'.$wgScriptPath.'/extensions/Shariff/shariff.min.css" />');
+	$out->addScript('<script type="text/javascript" src="'.$wgScriptPath.'/extensions/Shariff/shariff.min.js"></script>');
+	return $out;
+}
+
+ 
+function shariffLikeParserFunction_Render( &$parser, $param1 = '', $param2 = '', $param3 = '' ) {
+	global $wgSitename;
+	global $wgScriptPath;
+	
+	//Get page title and URL
+	$output = '<div class="shariff" data-backend-url="'.$wgScriptPath.'/extensions/Shariff/shariff-backend/"></div>';
+
+	return $parser->insertStripItem($output, $parser->mStripState);;
+}
