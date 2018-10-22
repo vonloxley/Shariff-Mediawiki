05d2798d4772d080972df4bf9a0db4e8a6560a74
Niki Hansche <mf-developing@hansche.de>
2018-10-22 09:12:14 +0200

    Convert to wfLoadExtension

--- Shariff.php
+++ Shariff.php
@@ -2,63 +2,42 @@
 /*
  * Parser that inserts social media buttons
  *
- * For more info see http://mediawiki.org/wiki/Extension:TwitterFBLike
+ * For more info see http://mediawiki.org/wiki/Shariff
  *
  * @package MediaWiki
  * @subpackage Extensions
  * @author Niki Hansche
- * @copyright © 2014 Niki Hansche
+ * @copyright © 2014-2018 Niki Hansche
  * @licence The MIT License (MIT)
  */
 
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'Shariff', 
-	'version' => '1.0',
-	'author' => 'Niki Hansche', 
-	'url' => 'https://github.com/vonloxley/Shariff-Mediawiki',
-	'description' => 'Insert 2-click "Like" buttons.',
-	'license-name' => 'MIT'
-);
+class Shariff {
+	static function shariffLikeParserFunction_Setup( &$parser ) {
+		# Set a function hook associating the "shariffLike_parser" magic word with our function
+		$parser->setFunctionHook( 'shariffLike', 'Shariff::shariffLikeParserFunction_Render' );
+		return true;
+	}
 
-$wgResourceModules['ext.Shariff'] = array(
-	'scripts' => 'shariff.min.js',
-	'styles' => 'shariff.min.css',
-	'position' => 'bottom',
+	static function shariffLikeParserFunction_Magic( &$magicWords, $langCode ) {
+	        //Set first parameter to 1 to make it case sensitive
+		$magicWords['shariffLike'] = array( 0, 'ShariffLike' );
+	        return true;
+	}
 
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'Shariff',
-);
+	static function shariffLikeParserFeedHead(&$out, &$sk) {
+		$out->addModules( 'ext.Shariff' );
 
-$wgHooks['ParserFirstCallInit'][] = 'shariffLikeParserFunction_Setup';
-$wgHooks['LanguageGetMagic'][]       = 'shariffLikeParserFunction_Magic';
-$wgHooks['BeforePageDisplay'][] = 'shariffLikeParserFeedHead';
-
-function shariffLikeParserFunction_Setup( &$parser ) {
-	# Set a function hook associating the "shariffLike_parser" magic word with our function
-	$parser->setFunctionHook( 'shariffLike', 'shariffLikeParserFunction_Render' );
-	return true;
-}
- 
-function shariffLikeParserFunction_Magic( &$magicWords, $langCode ) {
-        //Set first parameter to 1 to make it case sensitive
-	$magicWords['shariffLike'] = array( 0, 'ShariffLike' );
-        return true;
-}
-
-function shariffLikeParserFeedHead(&$out, &$sk) {
-	$out->addModules( 'ext.Shariff' );
-
-	return true;
-}
+		return true;
+	}
 
  
-function shariffLikeParserFunction_Render( &$parser, $param1 = '', $param2 = '', $param3 = '' ) {
-	global $wgSitename;
-	global $wgScriptPath;
-	
-	//Get page title and URL
-	$output = '<div class="shariff noprint" data-backend-url="'.$wgScriptPath.'/extensions/Shariff/shariff-backend/" data-services="[&quot;twitter&quot;,&quot;facebook&quot;,&quot;pinterest&quot;]"></div>';
+	static function shariffLikeParserFunction_Render( &$parser, $param1 = '', $param2 = '', $param3 = '' ) {
+		global $wgSitename;
+		global $wgScriptPath;
 
-	return $parser->insertStripItem($output, $parser->mStripState);;
+		//Get page title and URL
+		$output = '<div class="shariff noprint" data-backend-url="'.$wgScriptPath.'/extensions/Shariff/shariff-backend/" data-services="[&quot;twitter&quot;,&quot;facebook&quot;,&quot;pinterest&quot;]"></div>';
+
+		return $parser->insertStripItem($output, $parser->mStripState);;
+	}
 }
