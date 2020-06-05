23d7324faa64cd448dc1d15ec56e4744ed2812f5
Bernhard Krabina <krabina@kdz.or.at>
2020-06-05 19:57:54 +0200

    acknowledge de-formal, de_AT and de_CH as "de"

--- Shariff.php
+++ Shariff.php
@@ -7,7 +7,7 @@
  * @package MediaWiki
  * @subpackage Extensions
  * @author Niki Hansche
- * @copyright © 2014-2018 Niki Hansche
+ * @copyright Â© 2014-2018 Niki Hansche
  * @licence The MIT License (MIT)
  */
 
@@ -34,9 +34,17 @@ class Shariff {
 	static function shariffLikeParserFunction_Render( &$parser, $param1 = '', $param2 = '', $param3 = '' ) {
 		global $wgSitename;
 		global $wgScriptPath;
+		global $wgLanguageCode;
 
+		if ($wgLanguageCode == "de-formal" or $wgLanguageCode == "de-AT" or $wgLanguageCode == "de-CH" ) {
+			$datalang = "de";
+			}
+		else {
+			$datalang = $wgLanguageCode;
+		}
+		
 		//Get page title and URL
-		$output = '<div class="shariff noprint" data-backend-url="'.$wgScriptPath.'/extensions/Shariff/shariff-backend/" data-services="[&quot;twitter&quot;,&quot;facebook&quot;,&quot;pinterest&quot;]"></div>';
+		$output = '<div class="shariff noprint" data-lang="'.$datalang.'" data-backend-url="'.$wgScriptPath.'/extensions/Shariff/shariff-backend/" data-services="[&quot;twitter&quot;,&quot;facebook&quot;,&quot;pinterest&quot;]"></div>';
 
 		return $parser->insertStripItem($output, $parser->mStripState);;
 	}
